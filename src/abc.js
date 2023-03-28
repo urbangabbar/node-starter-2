2e96ef46332fbf5cb7f4b31c45b7a619548a187e
Abhinav Paliwal <55943450+urbangabbar@users.noreply.github.com>
2023-03-28 20:27:17 +0530

    http server

--- src/abc.js
+++
@@ -1,5 +0,0 @@
-const a = 10;
-const b = 20;
-
-module.exports.a = a;
-module.exports.b = b;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-const add = require('./maths');
-const {a, b} = require('./abc');
+var http = require('http'); // importing built http module
 
-console.log(add(a, b));
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello from chankya uni!'); //write a response to the client
+  res.end(); //end the response
+}).listen(3000);
+
+// http packahges are non use friendly
+// so there is a need of framework like     
