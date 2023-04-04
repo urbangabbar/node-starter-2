f40b88df2637c58c6cf813c9b813825cd84ac829
Abhinav Paliwal <55943450+urbangabbar@users.noreply.github.com>
2023-04-04 20:58:19 +0530

    COMPLETED SET OF APIS

--- src/server.js
+++ src/server.js
@@ -1,25 +1,78 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 //express has a concept of middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const hotels = []
+const hotels = [
+  {
+    name: "abhinav hotel 2",
+    id: "2",
+    price: 1000,
+  },
+  {
+    name: "abhinav hotel",
+    id: "1",
+    price: 1000,
+  },
+];
 
-app.get('/api/hotel',(req, res)=> {
-    return res.send(hotels)
+app.get("/api/hotel", (req, res) => {
+  return res.send(hotels);
 });
 
-app.post('/api/hotel', (req, res)=> {
-    const hotel = req.body;
-    hotels.push(hotel)
-    return res.status(201).send({
-        "msg": "succes hotel created"
-    });
-})
+app.get("/api/hotel/:hotelId", (req, res) => {
+  const { hotelId } = req.params;
+  const hotel = hotels.find((el) => el.id === hotelId);
+  if (hotel) {
+    return res.send(hotel);
+  } else {
+    return res
+      .status(404)
+      .send({ error: "Hotel not found. please try with a valid ID" });
+  }
+});
 
-app.listen(3000, ()=> {
-    console.log("Server has started on port 3000")
-})
+app.post("/api/hotel", (req, res) => {
+  const hotel = req.body;
+  hotels.push(hotel);
+  return res.status(201).send({
+    msg: "succes hotel created",
+  });
+});
+
+app.delete("/api/hotel/:hotelId", (req, res) => {
+  const { hotelId } = req.params;
+  const hotelIndex = hotels.findIndex((hotel) => hotel.id === hotelId);
+  if (hotelIndex === -1) {
+    return res
+      .status(404)
+      .send({ error: "Hotel not found. please try with a valid ID" });
+  }
+  hotels.splice(hotelIndex, 1);
+  return res.status(200).send({ msg: "Hotel removed successfully" });
+});
+
+app.put("/api/hotel/:hotelId", (req, res) => {
+  const { hotelId } = req.params;
+  const hotel = req.body;
+
+  const hotelIndex = hotels.findIndex((hotel) => hotel.id === hotelId);
+  if (hotelIndex === -1) {
+    return res
+      .status(404)
+      .send({ error: "Hotel not found. please try with a valid ID" });
+  }
+  hotels[hotelIndex] = {
+    name: hotel.name,
+    price: hotel.price,
+    id: hotelId,
+  };
+  return res.status(200).send(hotels[hotelIndex]);
+});
+
+app.listen(3000, () => {
+  console.log("Server has started on port 3000");
+});
